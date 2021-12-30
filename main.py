191e9e39790b274ec02daa7aed05b81c4c0b3296
GK <jpkutner+github@gmail.com>
2021-12-30 14:52:46 -0600

    now has favorite color and uses discord client

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 from os import getenv
+import random
 
 print("Booting up")
 
 load_dotenv()
 
-bot = commands.Bot(command_prefix='.')
+class MyClient(discord.Client):
+	async def on_ready(self):
+		print('Logged in as')
+		print(self.user.name)
+		print(self.user.id)
+		print('------')
 
-@bot.command()
-async def hello(ctx):
-	await ctx.reply('Hello!')
+	async def on_message(self, message):
+		# we do not want the bot to reply to itself
+		if message.author.id == self.user.id:
+			return
 
-@bot.command()
-async def add(ctx, num1:int, num2:int):
-	await ctx.reply(num1+num2)
+		for mention in message.mentions:
+			if self.user.id == mention.id:
+				await parse_message(message)
+			else:
+				print("????")
+				print(message)
 
-bot.run(getenv("TOKEN"))
+async def parse_message(message):
+	if "favorite color" in message.content.lower():
+		await message.reply(favorite_color(), mention_author=True)
+	else:
+		await message.reply("Is this a trick question?", mention_author=True)
+
+def favorite_color():
+	colors = ["Blue",'Red',"Green","Brown","Fuchsia","Bleu cheese","Baby blue","Maroon","Lavender","Gray","Metal","Black. Oh yeah; that's not a color.","Beige","Tan", "Orange", "Yellow", "Amber", "I like all colors. In other words, I like white.", "Blueberry", "Blackberry. Wait; black isn't a color!", "Blue...wait no, green!"]
+	color = random.choice(colors)
+	return color
+
+client = MyClient()
+
+client.run(getenv("TOKEN"))
